eef2a94e1b0665c95186362d0ecbf6bccff41408
Magnus Larsen <kontakt@magnuslarsen.dk>
2017-09-19 18:06:57 +0200

    changes to server

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 'use strict';
 
 const express = require('express');
-
+const cors = require('cors');
 // Constants
-const PORT = 8080;
+const PORT = 8081;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Krissen er en fed homoseksuel bög, og team boner er nogle fede noobs\n');
+
+
+
+
+app.options('/', cors());
+
+app.get('/', cors(), (req, res) => {
+
+    res.json({
+        msg: 'Krissen er en fed homoseksuel bög, og team boner er nogle fede noobs'
+    });
 });
 
 app.listen(PORT, HOST);
