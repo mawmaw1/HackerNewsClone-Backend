2692100c2dfafea514eb17c553ab86210f33e80c
CBorum <christopherborum@gmail.com>
2017-10-30 10:30:53 +0100

    kuk

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const passport = require('passport')
+
 const api = require('./router')
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
-mongoose.connect('mongodb://localhost:27017/hackernews', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/hackernews', { useMongoClient: true })
 mongoose.Promise = global.Promise
 
 // App
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(morgan('short'))
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'meownus',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
     res.json({
         msg: 'Hej fra backend... klokken er ' + new Date().toLocaleString()
-    });
-});
+    })
+})
 
 app.use(api)
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
